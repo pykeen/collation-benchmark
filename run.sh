5e069bdb7c7b121a6500964487dd34820014dc00
Max Berrendorf <berrendorf@dbs.ifi.lmu.de>
2022-02-01 09:04:15 +0000

    update run.sh with local paths

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 set -e
-git -C /Users/cthoyt/dev/pykeen checkout master
-python /Users/cthoyt/dev/collation-benchmark/main.py --top 4
-git -C /Users/cthoyt/dev/pykeen checkout negative-sampling-in-data-loader
-python /Users/cthoyt/dev/collation-benchmark/main.py --top 4
-python /Users/cthoyt/dev/collation-benchmark/compare.py
+git -C ../pykeen checkout master
+python main.py --top 4
+git -C ../pykeen checkout negative-sampling-in-data-loader
+python main.py --top 4
+python compare.py
 #git commit --all -m "Ran benchmark"
 #git push
